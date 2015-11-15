007965ba1c8f808a487806f373ea0801f4bacbcf
Jigar <jigar.shar24@gmail.com>
2015-11-15 00:30:58 -0500

    initialized constants for mongo database

---
+++ Constants.py
@@ -0,0 +1,7 @@
+class Settings:
+    def __init__(self):
+        pass
+
+    JSON_DATASET_FILE = 'F:\NCSU\CSC522\Project\yelp_dataset_challenge_academic_dataset'
+    MONGO_URI = "mongodb://localhost:27030/"
+    
