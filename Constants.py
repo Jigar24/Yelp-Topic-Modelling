85507aa384647cf80d8dda16e940f1adf24c3daf
Jigar <jigar.shar24@gmail.com>
2015-11-15 13:59:26 -0500

    added Collection Constants

--- Constants.py
+++ Constants.py
@@ -1,7 +1,9 @@
-class Settings:
+class constants:
     def __init__(self):
         pass
 
-    JSON_DATASET_FILE = 'F:\NCSU\CSC522\Project\yelp_dataset_challenge_academic_dataset'
-    MONGO_URI = "mongodb://localhost:27030/"
-    
+    JSON_DATASET_FILE = 'F:/NCSU/CSC522/Project/yelp_dataset_challenge_academic_dataset/'
+    MONGO_URI = "mongodb://localhost:27017/"
+    DATABASE = "Yelp_User_Reviews"
+    REVIEWS_COLLECTION = "Reviews"
+    BUSINESS_COLLECTION = "Business"
