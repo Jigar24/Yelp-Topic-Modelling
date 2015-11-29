9b71f657066314054f441cbcfcbcebf9e21a4cc1
rpatel17 <rpatel17@ncsu.edu>
2015-11-29 14:29:19 -0800

    changed path

--- Constants.py
+++ Constants.py
@@ -2,7 +2,7 @@ class constants:
     def __init__(self):
         pass
 
-    JSON_DATASET_FILE = 'F:/NCSU/CSC522/Project/yelp_dataset_challenge_academic_dataset/'
+    JSON_DATASET_FILE = 'C:/Users/Ronak Patel/Desktop/CSC522/Project/topics-master/yelp/yelp_dataset_challenge_academic_dataset/'
     MONGO_URI = "mongodb://localhost:27017/"
     DATABASE = "Yelp_User_Reviews"
     REVIEWS_COLLECTION = "Reviews"
