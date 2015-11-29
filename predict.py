98a79210e38d4772ed1dbe8e877395133c476b49
Jigar <jigar.shar24@gmail.com>
2015-11-29 13:23:19 -0500

    modified script for predcting topics

--- predict.py
+++ predict.py
@@ -8,12 +8,18 @@ from gensim import corpora
 import nltk
 from nltk.stem.wordnet import WordNetLemmatizer
 
+db = MongoClient()[constants.DATABASE]
+topic_rating_collection = MongoClient()[constants.DATABASE][constants.TOPIC_RATING_COLLECTION]
 restaurants_collection = MongoClient()[constants.DATABASE][constants.BUSINESS_COLLECTION]
 reviews_collection = MongoClient()[constants.DATABASE][constants.REVIEWS_COLLECTION]
 review_cursor = reviews_collection.find()
 corpus_collection = MongoClient()[constants.DATABASE][constants.CORPUS_COLLECTION]
 cursor = corpus_collection.find()
-f = open('latent_ratings.txt','w')
+
+print topic_rating_collection.find().count()
+#db.drop_collection(topic_rating_collection)
+
+#f = open('latent_ratings.txt','w')
 class Predict():
     def __init__(self):
         dictionary_path = "Data/Dict"
@@ -21,56 +27,35 @@ class Predict():
         self.dictionary = corpora.Dictionary.load(dictionary_path)
         self.lda = LdaModel.load(lda_model_path)
 
-    def load_stopwords(self):
-        stopwords = {}
-        with open('stopwords.txt', 'rU') as f:
-            for line in f:
-                stopwords[line.strip()] = 1
-
-        return stopwords
-
-    def extract_lemmatized_nouns(self, new_review):
-        stopwords = self.load_stopwords()
-        words = []
-
-        sentences = nltk.sent_tokenize(new_review.lower())
-        for sentence in sentences:
-            tokens = nltk.word_tokenize(sentence)
-            text = [word for word in tokens if word not in stopwords]
-            tagged_text = nltk.pos_tag(text)
-
-            for word, tag in tagged_text:
-                words.append({"word": word, "pos": tag})
-
-        lem = WordNetLemmatizer()
-        nouns = []
-        for word in words:
-            if word["pos"] in ["NN", "NNS"]:
-                nouns.append(lem.lemmatize(word["word"]))
-
-        return nouns
-
-    def run(self, new_review , i):
-       # nouns = self.extract_lemmatized_nouns(new_review)
-		new_review_bow = self.dictionary.doc2bow(cursor.__getitem__(i)['words'])
+    def run(self,i):
+		review = cursor.__getitem__(i)
+		new_review_bow = self.dictionary.doc2bow(review['words'])
 		new_review_lda = self.lda[new_review_bow]
 		
-		print new_review_lda
+		print i
+		
 		#f.write(i'\t')
-		f.write(str(new_review_lda))
-		f.write('\n')
+		topic_rating_collection.update_one({'_id':review['_id']},{"$set":{
+		"_id" : review['_id'],
+		"business_id" : review['business_id'],
+		"user_id" : review ["user_id"],
+		"stars" : review["stars"],
+		"rating" : new_review_lda
+		}})
+		#f.write(str(new_review_lda))
+		#f.write('\n')
 
-
+predict = Predict()
 def main():
 	#logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 	
 	for i in range(cursor.count()):
-		corpus_review = cursor.__getitem__(i)
-		#print (corpus_review['_id'])
-		review_id = reviews_collection.find_one( {'_id': corpus_review['_id'] } )
-		new_review = review_id['review_text']
-		predict = Predict()
-		predict.run(new_review, i)
+		#corpus_review = cursor.__getitem__(i)
+		#print (corpus_review)
+		#review_id = reviews_collection.find_one( {'_id': corpus_review['_id'] } )
+		#new_review = review_id['review_text']
+		#predict = Predict()
+		predict.run(i)
 		
 
 
