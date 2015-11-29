5d058b52b43671b0519c703fedbee44992dfe04b
Jigar <jigar.shar24@gmail.com>
2015-11-29 13:47:23 -0500

    script to compute perplexity

--- check.py
+++ check.py
@@ -5,15 +5,12 @@ from UTFToAscii import convert
 from pymongo import MongoClient
 from Constants import constants
 
-
 restaurants_collection = MongoClient()[constants.DATABASE][constants.BUSINESS_COLLECTION]
-reviews_collection = MongoClient()[constants.DATABASE][constants.REVIEWS_COLLECTION]
 corpus_collection = MongoClient()[constants.DATABASE][constants.CORPUS_COLLECTION]
+topic_rating_collection = MongoClient()[constants.DATABASE][constants.TOPIC_RATING_COLLECTION]
 corpus_cursor = corpus_collection.find()
 restaurant_cursor = restaurants_collection.find()
-review_cursor = reviews_collection.find()
-#rest = restaurants_collection.find()
-#print (reviews_collection.find_one()['_id'])
+topic_rating_cursor = topic_rating_collection.find()
 
 for i in range(restaurant_cursor.count()):
 	try:		
@@ -22,21 +19,14 @@ for i in range(restaurant_cursor.count()):
 		reviews = corpus_collection.find({'business_id': rest['_id']})
 		print ('asdf')
 		print (reviews.count())
-		#for review in reviews:
-			#print (review['_id'])
+		for review in reviews:
+			print (review['_id'])
+			topic_rating = topic_rating_collection.find_one({'_id' : review['_id']})
+			print topic_rating
 	except Exception:
 		print ('Exception')
 		continue
 
-
-
-with open(constants.JSON_DATASET_FILE+'yelp_academic_dataset_business.json') as business:
-	for line in business:
-		business_info = json.loads(line)
-
-val = restaurants_collection.find_one( {"_id" : "SQ0j7bgSTazkVQlF5AnqyQ" })
-
-#print convert(review)
 
 		
 		
