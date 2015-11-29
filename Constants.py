c267df6efcdd305c4ef4862768f683540c66909c
sasanghavi <sasanghavi@gmail.com>
2015-11-28 20:34:08 -0500

    User Info Database Added

--- Constants.py
+++ Constants.py
@@ -2,9 +2,11 @@ class constants:
     def __init__(self):
         pass
 
-    JSON_DATASET_FILE = 'F:/NCSU/CSC522/Project/yelp_dataset_challenge_academic_dataset/'
+    #JSON_DATASET_FILE = 'F:/topics-master/yelp_dataset_challenge_academic_dataset/'
+	JSON_DATASET_FILE = 'F:/NCSU/CSC522/Project/yelp_dataset_challenge_academic_dataset/'
     MONGO_URI = "mongodb://localhost:27017/"
     DATABASE = "Yelp_User_Reviews"
     REVIEWS_COLLECTION = "Reviews"
     BUSINESS_COLLECTION = "Business"
     CORPUS_COLLECTION = "Corpus"
+    USER_COLLECTION = "User"
